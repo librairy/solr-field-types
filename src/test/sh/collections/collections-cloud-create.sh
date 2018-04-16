89b8107eb1871fb6f4c0fe8fc232a5e4eb3448d4
Carlos Badenes <cbadenes@gmail.com>
2018-04-16 10:28:06 +0200

    solr cloud management

---
+++ src/test/sh/collections/collections-cloud-create.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+../../../../solr-7.2.1/bin/solr create_collection -c CORDIS_ -d setsi -shards 1 -replicationFactor 1
---
+++ src/test/sh/server/solr-start-import.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+../../../../solr-7.2.1/bin/solr start -e dih
+../../../../solr-7.2.1/bin/solr start -e dih
